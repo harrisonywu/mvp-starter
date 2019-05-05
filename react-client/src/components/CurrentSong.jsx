3b7213f4d464fc40a8ff013c070c499ec5cd358d
Harrison Wu <harrisonywu@gmail.com>
2019-05-04 19:42:34 -0700

    Add heading to Current Song section

--- react-client/src/components/CurrentSong.jsx
+++ react-client/src/components/CurrentSong.jsx
@@ -5,6 +5,7 @@ function CurrentSong(props) {
   if (typeof props.currentSongInfo != 'undefined') {
     return (
       <div>
+        <h1>CURRENT SONG</h1>
         <div>{props.currentSongInfo.name} by {props.currentSongInfo.album.artists[0].name}</div>
         {console.log(props.currentSongInfo.album.images.url)}
         <img src={props.currentSongInfo.album.images[2].url}></img>
@@ -13,6 +14,7 @@ function CurrentSong(props) {
   } else {
     return (
       <div>
+        <h1>CURRENT SONG</h1>
         No Song currently playing.
       </div>
     )
