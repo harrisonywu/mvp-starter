a0c85db2fecf6f57ba1f05856820b34417a70c60
Harrison Wu <harrisonywu@gmail.com>
2019-05-24 14:18:02 -0700

    Prevent duplicate saves in database

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -31,18 +31,33 @@ var selectAll = function(callback) {
 };
 
 var saveOne = function(data, callback) {
-  var newEntry = new Song({ 
+  let songObj = { 
     songName: data.name,
     artistName: data.artists[0].name,
     albumName: data.album.name,
     albumImg: data.album.images[2].url,
-  })
-  newEntry.save((err) => {
-    if(err) {
-      console.log('error saving data: ', err)
-    }
-  })
+  } 
   
+  var newEntry = new Song( songObj)
+  
+  Song.findOne(songObj, (err, response) => {
+    if (err) {
+      console.log('error finding one: ', err)
+    } else {
+      if (!response) {
+        newEntry.save((err) => {
+          if (err) {
+            console.log('error saving data: ', err)
+          } else {
+            console.log('song data saved');
+            callback()
+          }
+        });
+      } else {
+        console.log(`${data.name} by ${data.artists[0].name} is already saved!`)
+      }
+    }
+  }) 
 }
 
 module.exports.selectAll = selectAll;
